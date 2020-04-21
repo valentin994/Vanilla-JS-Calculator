4922262fb7b28bfe65a14206e4ebaca218ac9e35
Valentin Vareskic <valentin.vareskic@outlook.com>
2020-04-21 13:36:31 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".keys");
+const display = document.querySelector('.display')
+console.log(display.textContent);
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
+
+        if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent
+                calculator.dataset.previousKeyType = 'number'
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+        }
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.'
+        }
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
+
+        }
+        if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+
+            display.textContent = calculate(firstValue, operator, secondValue)
+        }
+
+        if (action === 'clear') {
+            calculator.dataset.firstValue = ''
+            calculator.dataset.previousKeyType = ''
+            calculator.dataset.operator = ''
+        }
+
+    }
+})
+
+const calculate= (n1, operator, n2) => {
+    let result = ''
+  
+    if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'subtract') {
+      result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2)
+    }
+    
+    return result
+}
